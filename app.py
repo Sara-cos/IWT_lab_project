1c2657060de7c486db89db91b8d0d16f76e82c70
Subh-github <subhamkumarsaoo12345@gmail.com>
2022-05-28 23:09:38 +0530

    pushing

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ config.mongo_db = Operations("ExamPortal", config.logger)
 
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
-    return render_template('Dashboard.html')
+    return render_template('Home.html')
 
 
 @app.route('/auth/login', methods=['GET', 'POST'])
