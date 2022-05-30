963a850828936b5155dc915945e4312f0d6875d8
Shubham Gantayat <sgantayat9@gmail.com>
2022-05-30 12:40:53 +0530

    repo functions created

--- py_backend/questions/questions.py
+++ py_backend/questions/questions.py
@@ -7,17 +7,19 @@ class Question:
     def insert_a_question(self, record):
         data = dict(
             question_id = str(uuid1()),
-            question = record["question"],
-            option1 = record["option1"],
-            option2 = record["option2"],
-            option3 = record["option3"],
-            option4 = record["option4"],
-            answer = record["answer"],
-            exam_id = record["exam_id"]
+            question = record["Question"],
+            option1 = record["Option-1"],
+            option2 = record["Option-2"],
+            option3 = record["Option-3"],
+            option4 = record["Option-4"],
+            answer = int(record["Correct Option"]),
+            exam_id = record["Exam ID"]
         )
         config.mongo_db.insert_one("questions", data)
 
     def get_all_questions(self, exam_id):
         result = config.mongo_db.my_db['questions'].find({"exam_id": exam_id})
         return result
+
+    
 
